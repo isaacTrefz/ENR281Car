098916f5345f6e048240e7f637ce2ab6bb49d20e
isaacTrefz <isaac.h.trefz@gmail.com>
2024-03-07 17:48:39 -0600

    Cleaned up files

--- LED_PWM_Example_1/LED_PWM_Example_1.ino
+++ LED_PWM_Example_1/LED_PWM_Example_1.ino
@@ -1,8 +1,54 @@
-// the number of the LED pin
-const int ledPin = 16;  // 16 corresponds to GPIO16
+// Include the ESP32 PWM library
+//#include <ledc.h>
+
+// Define the PWM settings
+#define PWM_CHANNEL 0
+#define PWM_FREQ    50 // 50Hz
+#define PWM_RESOLUTION 10 // 10-bit resolution for duty cycle
+
+// Define the pin for PWM output
+#define PWM_PIN 22
+
+void setup() {
+  // Configure PWM channel
+  ledcSetup(PWM_CHANNEL, PWM_FREQ, PWM_RESOLUTION);
+  
+  // Attach the PWM channel to the GPIO pin
+  ledcAttachPin(PWM_PIN, PWM_CHANNEL);
+}
+
+void loop() {
+  // Set duty cycle to 10%
+  int dutyCycle = 1023 * 0.1; // 10% of the maximum duty cycle (1023 for 10-bit resolution)
+  
+  // Write duty cycle value to the PWM channel
+  ledcWrite(PWM_CHANNEL, dutyCycle);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*// the number of the LED pin
+const int ledPin = 22;  // 16 corresponds to GPIO16
 
 // setting PWM properties
-const int freq = 5000;
+const int freq = 50;
 const int ledChannel = 0;
 const int resolution = 8;
  
@@ -16,16 +62,18 @@ void setup(){
  
 void loop(){
   // increase the LED brightness
-  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
+  //for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
     // changing the LED brightness with PWM
-    ledcWrite(ledChannel, dutyCycle);
-    delay(15);
-  }
+  //  ledcWrite(ledChannel, dutyCycle);
+   // delay(15);
+  //}
+
+ledcWrite(ledChannel, 25);
 
   // decrease the LED brightness
-  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
+//  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
     // changing the LED brightness with PWM
-    ledcWrite(ledChannel, dutyCycle);   
-    delay(15);
-  }
-}
+  //  ledcWrite(ledChannel, dutyCycle);   
+    //delay(15);
+  //}
+}*/
